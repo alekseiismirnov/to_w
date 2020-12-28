032597268af19fe6b023ba89556dec0b51173ecc
Alekseii Smirnov <alekseii.sm@gmail.com>
2020-12-29 00:08:21 +0200

    ones

--- lib/to_w.rb
+++ lib/to_w.rb
@@ -1,6 +1,11 @@
+ONES = {
+  1=>'one', 2=>'two', 3=>'three', 4=>'four', 5=>'five',
+  6=>'six', 7=>'seven', 8=>'eight', 9=>'nine'
+}
+
 class Integer
   def to_w
-    'one'
+    ONES[self]
   end
 end
 
--- spec/to_w_spec.rb
+++ spec/to_w_spec.rb
@@ -5,4 +5,12 @@ describe 'Integer#to_w' do
   it 'converts 1 to one' do
     expect(1.to_w).to eq('one')
   end
+
+  it 'converts 2 to two' do
+    expect(2.to_w).to eq('two')
+  end
+
+  it 'converts 9 to nine' do
+    expect(9.to_w).to eq('nine')
+  end
 end
